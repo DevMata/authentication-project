a9d5a739ef7225d2bca3a5b076ab1eb481cf936b
Antonio Mata <hildeamp@gmail.com>
2019-12-20 21:17:46 -0600

    :sparkles: Add auth service logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async validateUser(username: string, pass: string): Promise<{ userId: number; username: string } | null> {
+    const user = await this.usersService.findOne(username);
+
+    if (user && user.password === pass) {
+      const { userId, username } = user;
+      return { userId, username };
+    }
+    return null;
+  }
+}
