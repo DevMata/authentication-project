46021bcf709f1dec3d8eb19ae081940d000c4bb8
Antonio Mata <hildeamp@gmail.com>
2019-12-21 17:26:28 -0600

    :sparkles: Add profile get route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Request, UseGuards, Post } from '@nestjs/common';
+import { Controller, Request, UseGuards, Post, Get, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth/auth.service';
 import { AccessToken } from './auth/interfaces/accessToken.interface';
@@ -11,5 +11,11 @@ export class AppController {
   @Post('auth/login')
   async login(@Request() req): Promise<AccessToken> {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Req() req): string {
+    return req.user;
   }
 }
