5ce1beff2ae96fede392174db9d072c7b051b7d2
Antonio Mata <hildeamp@gmail.com>
2019-12-20 21:18:03 -0600

    :recycle: Refactor auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule],
 })
 export class AuthModule {}
