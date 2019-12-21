0e0aba3d2704b9786e7550aa389205298068b4e7
Antonio Mata <hildeamp@gmail.com>
2019-12-21 17:02:56 -0600

    :recycle: Use JWT login method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Request, UseGuards, Post } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { AccessToken } from './auth/interfaces/accessToken.interface';
 
 @Controller()
 export class AppController {
+  constructor(private readonly authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
-  async login(@Request() req): Promise<string> {
-    return req.user;
+  async login(@Request() req): Promise<AccessToken> {
+    return this.authService.login(req.user);
   }
 }
